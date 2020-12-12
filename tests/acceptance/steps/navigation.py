13ae6a8564a8531b498cd71b8959b4e03e25cb45
Jesús M <jssmtr@gmail.com>
2020-12-12 11:08:54 +0100

    browser App testada con selenium

---
+++ tests/acceptance/steps/navigation.py
@@ -0,0 +1,22 @@
+from behave import *
+from selenium import webdriver
+
+use_step_matcher('re')
+
+
+@given('I am on the homepage')
+def step_impl(context):
+    context.browser = webdriver.Firefox(executable_path="C:/Users/geckodriver.exe")
+    context.browser.get('http://127.0.0.1:5000')
+
+@given('I am on the blog page')
+def step_impl(context):
+    context.browser = webdriver.Firefox(executable_path="C:/Users/geckodriver.exe")
+    context.browser.get('http://127.0.0.1:5000')
+
+@then('I am on the blog page')
+def step_impl(context):
+    expected_url = 'http://127.0.0.1:5000/blog'
+    assert context.browser.current_url == expected_url
+
+# Estas funciones se llaman igual, pero no hay problema porque ambas tienen decoradores
