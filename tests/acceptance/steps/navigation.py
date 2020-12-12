ef420cbde26f300f1b31e9531a74e94b58c47c32
Jesús M <jssmtr@gmail.com>
2020-12-12 11:23:24 +0100

    browser App testada con selenium Scenario: abrir homepage, ir a blog page Scenario: abrir blog page, ir a homepage

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -12,11 +12,16 @@ def step_impl(context):
 @given('I am on the blog page')
 def step_impl(context):
     context.browser = webdriver.Firefox(executable_path="C:/Users/geckodriver.exe")
-    context.browser.get('http://127.0.0.1:5000')
+    context.browser.get('http://127.0.0.1:5000/blog')
 
 @then('I am on the blog page')
 def step_impl(context):
     expected_url = 'http://127.0.0.1:5000/blog'
+    assert context.browser.current_url == expected_url
+
+@then('I am on the homepage')
+def step_impl(context):
+    expected_url = 'http://127.0.0.1:5000/'
     assert context.browser.current_url == expected_url
 
 # Estas funciones se llaman igual, pero no hay problema porque ambas tienen decoradores
